2ca83b4a041450b158d3cc944f698092cced756f
Thiago Sueto <herzenschein@gmail.com>
2021-02-25 22:56:02 -0300

    Make vars inline

--- src/furvars.h
+++ src/furvars.h
@@ -1,10 +1,9 @@
 #ifndef FURVARS_H
 #define FURVARS_H
-#include <QString>
 
-static int comfyVar;// = 50;
-std::string comfyString;
-std::string nameCharacter;
-std::string speciesCharacter;
+inline int comfyVar;// = 50;
+inline std::string comfyString;
+inline std::string nameCharacter;
+inline std::string speciesCharacter;
 
 #endif // FURVARS_H
